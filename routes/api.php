3b0acf5bd9d9bdc6657bb7be7a315e0c52fcc396
sethunathan <sethunathan@users.noreply.github.com>
2025-09-15 17:11:48 +0530

    post check

--- routes/api.php
+++ routes/api.php
@@ -22,3 +22,9 @@ Route::get('/test',function (Request $request) {
     return response()->json(['message' => 'Hello from Laravel API']);
 });
  
+Route::post('/hello', function (Request $request) {
+    return response()->json([
+        'message' => 'Hello POST received!',
+        'data' => $request->all()
+    ]);
+});
